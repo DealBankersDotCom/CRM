80f681e66294ff55d3fefb2ecddcffb30ab59506
DealBankersDotCom <dealbankers@gmail.com>
2025-08-05 06:27:17 -0500

    Update leads.js

--- leads.js
+++ leads.js
@@ -2,25 +2,34 @@ const leads = [
   {
     address: "8119 Comanche Pass, Converse, TX",
     contacts: [
-      { name: "ADRIAN MEJIA, MIREYA MEJIA", phone: "2106636283" }
+      { name: "Adrian Mejia", phone: "2106636283" }
     ]
   },
   {
     address: "7438 Bluebonnet Bay, San Antonio, TX 78218",
     contacts: [
-      { name: "HARWINDER KAUR KANG, GURJIT SINGH BAJWA", phone: "2104295130" }
+      { name: "Harwinder Kaur Kang", phone: "2104295130" },
+      { name: "Gurjit Singh Bajwa", phone: "2104295130" }
     ]
   },
   {
     address: "7535 Perseus Sound, San Antonio, TX 78252",
     contacts: [
-      { name: "SCOTT STEPHAN / Patricia Stephan 53 yo", phone: "2102607618" }
+      { name: "Patricia Stephan 53 yo", phone: "2102607618" }
+    ]
+  },
+  {
+    address: "206 Sprucewood Ln, San Antonio, TX 78216",
+    contacts: [
+      { name: "Eva Sankey 49 yo", phone: "2105442743" },
+      { name: "Rosanna Hill", phone: "2105442743" },
+      { name: "Brant Sankey knwn assc. 48 yo", phone: "2107220807" }
     ]
   },
   {
     address: "10527 Big Mesa St, Converse, TX 78109",
     contacts: [
-      { name: "DAVID A. WATTS / David Watts 78 yo", phone: "2108187899" }
+      { name: "David Watts 78 yo", phone: "2108187899" }
     ]
   },
 ];
