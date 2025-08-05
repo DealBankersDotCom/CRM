db1bfde4cc872653ca582b262e77fe34a71f5a54
DealBankersDotCom <dealbankers@gmail.com>
2025-08-05 06:02:30 -0500

    Create leads.js

---
+++ leads.js
@@ -0,0 +1,26 @@
+const leads = [
+  {
+    address: "8119 Comanche Pass, Converse, TX",
+    contacts: [
+      { name: "ADRIAN MEJIA, MIREYA MEJIA", phone: "2106636283" }
+    ]
+  },
+  {
+    address: "7438 Bluebonnet Bay, San Antonio, TX 78218",
+    contacts: [
+      { name: "HARWINDER KAUR KANG, GURJIT SINGH BAJWA", phone: "2104295130" }
+    ]
+  },
+  {
+    address: "7535 Perseus Sound, San Antonio, TX 78252",
+    contacts: [
+      { name: "SCOTT STEPHAN / Patricia Stephan 53 yo", phone: "2102607618" }
+    ]
+  },
+  {
+    address: "10527 Big Mesa St, Converse, TX 78109",
+    contacts: [
+      { name: "DAVID A. WATTS / David Watts 78 yo", phone: "2108187899" }
+    ]
+  },
+];
