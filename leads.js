b7944e71e7ee23740adfdec7cb6af002225ab629
DealBankersDotCom <dealbankers@gmail.com>
2025-08-10 09:53:33 -0500

    Update leads.js

--- leads.js
+++ leads.js
@@ -6,11 +6,23 @@ window.leads = [
       {
         "name": "Adrian Mejia",
         "phone": "(210) 663-6283"
-      },
+      }
+    ]
+  },
+  {
+    "address": "8119 Comanche Pass, Converse, TX",
+    "notes": "Owner occupied, built 1985, older wood exterior, long-term ownership, seller carryback loan in 1999",
+    "contacts": [
       {
         "name": "Adrian Mejia",
         "phone": "(210) 763-9988"
-      },
+      }
+    ]
+  },
+  {
+    "address": "8119 Comanche Pass, Converse, TX",
+    "notes": "Owner occupied, built 1985, older wood exterior, long-term ownership, seller carryback loan in 1999",
+    "contacts": [
       {
         "name": "Alfred Mejia",
         "phone": "(210) 371-2343"
@@ -24,11 +36,23 @@ window.leads = [
       {
         "name": "Shelley Olivier",
         "phone": "(210) 844-9141"
-      },
+      }
+    ]
+  },
+  {
+    "address": "16602 Hunting Valley St, San Antonio, TX 78247",
+    "notes": "Pool, dated 1974 build, equity potential, last FHA in 2015",
+    "contacts": [
       {
         "name": "Jared Olivier",
         "phone": "(210) 844-9174"
-      },
+      }
+    ]
+  },
+  {
+    "address": "16602 Hunting Valley St, San Antonio, TX 78247",
+    "notes": "Pool, dated 1974 build, equity potential, last FHA in 2015",
+    "contacts": [
       {
         "name": "Kaitlin Olivier",
         "phone": "(210) 844-1497"
@@ -42,7 +66,13 @@ window.leads = [
       {
         "name": "Paula Bazan",
         "phone": "(210) 716-4563"
-      },
+      }
+    ]
+  },
+  {
+    "address": "5582 Kissing Oak St, San Antonio, TX 78247",
+    "notes": "Older 1980 build, large 0.26-acre lot, potential equity, gifted deed",
+    "contacts": [
       {
         "name": "Joe Bazan",
         "phone": "(210) 716-4563"
@@ -86,11 +116,23 @@ window.leads = [
       {
         "name": "Eva Sankey",
         "phone": "(210) 544-2743"
-      },
+      }
+    ]
+  },
+  {
+    "address": "206 Sprucewood Ln, San Antonio, TX 78216",
+    "notes": "Asbestos siding, 1955 build, 2 beds on large lot, multiple past distressed events including foreclosure",
+    "contacts": [
       {
         "name": "Rosanna Hill Eva Sankey",
         "phone": "(210) 544-2743"
-      },
+      }
+    ]
+  },
+  {
+    "address": "206 Sprucewood Ln, San Antonio, TX 78216",
+    "notes": "Asbestos siding, 1955 build, 2 beds on large lot, multiple past distressed events including foreclosure",
+    "contacts": [
       {
         "name": "Brant Sankey knwn assc.",
         "phone": "(210) 722-0807"
