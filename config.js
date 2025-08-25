eb8ef84868b2096a02e46a8d176131ab0249da47
DealBankersDotCom <dealbankers@gmail.com>
2025-08-25 10:15:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
-window.firebaseConfig = {
-  apiKey: "...",
+<!-- config.js -->
+<script>
+// Expose one config object BEFORE any Firebase code runs.
+window.firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyAoidTIHZZwOJ_FrXXMZd_hPAuoxDJJncs",
   authDomain: "deal-bankers.firebaseapp.com",
   databaseURL: "https://deal-bankers-default-rtdb.firebaseio.com",
   projectId: "deal-bankers",
-  storageBucket: "deal-bankers.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
-};
+  storageBucket: "deal-bankers.appspot.com"
+  // messagingSenderId/appId optional for your use case
+});
+</script>
