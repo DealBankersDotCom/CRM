d4cf5b4d6a7e2111db6f31364aee1d102d4851b7
DealBankersDotCom <dealbankers@gmail.com>
2025-08-24 22:14:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.firebaseConfig = {
-  apiKey: "…",
+  apiKey: "...",
   authDomain: "deal-bankers.firebaseapp.com",
   databaseURL: "https://deal-bankers-default-rtdb.firebaseio.com",
   projectId: "deal-bankers",
   storageBucket: "deal-bankers.appspot.com",
-  messagingSenderId: "…",
-  appId: "…"
+  messagingSenderId: "...",
+  appId: "..."
 };
