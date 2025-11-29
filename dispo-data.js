f5cdaf6797d440bc6498a7751df32868a9a66d93
Deal Bankers <dealbankers@gmail.com>
2025-11-28 18:11:23 -0600

    Update dispo-data.js

--- dispo-data.js
+++ dispo-data.js
@@ -1,2 +1,32 @@
-// DealBankers • Dispo seed removed. We now load strictly from Firebase at `dispo/deals/{uid}`.
-window.DISPO_SEED = [];
+window.dispoDeals = [
+  {
+    address: "2900 & 2902 Vera Cruz, San Antonio, TX",
+    arv: "N/A",
+    repairs: "Tear down (2902), Two vacant lots",
+    photos: "https://photos.app.goo.gl/a7FzxtHnD4S6fFvR7",
+    folder: "vera-cruz-st",
+    images: ["1.png", "2.png", "3.png", "4.png"],
+    notes: "862 sq ft building on 2902 Vera cruz - tear down. Two vacant lots.",
+  },
+  {
+    address: "5218 Witt St, San Antonio, TX 78228",
+    arv: "$210K",
+    repairs: "$60K",
+    photos: "https://photos.app.goo.gl/ZRdq1Pedb1bg4Yo4A",
+    folder: "witt",
+    images: [
+      "575022920_5121671181391260_3396840921694873824_n.jpg",
+      "575443648_1125028422737494_6850675943801329200_n.jpg"
+    ],
+    notes: "Mid-level rehab. Good bones. Excellent rental hold or flip."
+  },
+  {
+    address: "7830 Stagecoach Dr, San Antonio, TX 78227",
+    arv: "$180K",
+    repairs: "$30K",
+    photos: "https://photos.app.goo.gl/wPx2HGiFQSVXTRXF7",
+    folder: "stagecoach",
+    images: ["IMG_20251121_152657342.jpg", "IMG_20251121_152705080.jpg"],
+    notes: "Active option on short sale in progress. Light to mid rehab needed."
+  }
+];
