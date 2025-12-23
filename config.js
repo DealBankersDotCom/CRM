b4a53a2261e64bbce09fdf57de1593e952131922
Deal Bankers <dealbankers@gmail.com>
2025-12-22 21:23:07 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js (NON-MODULE)
+
 window.firebaseConfig = {
   apiKey: "AIzaSyAoidTIHZZwOJ_FrXXMZd_hPAuoxDJJncs",
   authDomain: "deal-bankers.firebaseapp.com",
@@ -9,3 +10,6 @@ window.firebaseConfig = {
   appId: "1:485110155601:web:b4c38350ac58c1a2ecab70",
   measurementId: "G-KTNXZM14F7"
 };
+
+// Google Maps Embed key (client-side OK if HTTP referrer restricted to your domains)
+window.MAPS_EMBED_KEY = "AIzaSyAoidTIHZZwOJ_FrXXMZd_hPAuoxDJJncs";
