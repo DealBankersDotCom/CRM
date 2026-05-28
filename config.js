ab92cebc6dc90edfc38de54c82d3c0192746ad47
Deal Bankers <dealbankers@gmail.com>
2026-05-28 10:29:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,13 @@
 // config.js (NON-MODULE)
-
 window.firebaseConfig = {
   apiKey: "AIzaSyAoidTIHZZwOJ_FrXXMZd_hPAuoxDJJncs",
   authDomain: "deal-bankers.firebaseapp.com",
-  databaseURL: "https://deal-bankers-default-rtdb.firebaseio.com",
+  // databaseURL removed — Firestore doesn't need it
   projectId: "deal-bankers",
   storageBucket: "deal-bankers.firebasestorage.app",
   messagingSenderId: "485110155601",
   appId: "1:485110155601:web:b4c38350ac58c1a2ecab70",
   measurementId: "G-KTNXZM14F7"
 };
-
-// Wholesale portal Street View + map key:
 window.MAPS_JS_KEY = "AIzaSyC3bn6cYLdP53ITSPLO1jx_CmKIMOGO8Q4";
-
-// Contractor tracking map key:
 window.CONTRACTOR_MAPS_KEY = "AIzaSyBGTUG4NyhUUGh9MW1JFof1NLyeszhNOVk";
